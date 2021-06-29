936518f98313c6205a806ab81fc9af435f218900
Fare9 <eduardo.blazquez@uc3m.es>
2021-06-29 16:27:26 +0200

    Added dex disassembler class so user decides if wants to apply disassembly or not

---
+++ DEX/DVM/dex_disassembler.cpp
@@ -0,0 +1,119 @@
+#include "dex_disassembler.hpp"
+
+namespace KUNAI
+{
+    namespace DEX
+    {
+        DexDisassembler::DexDisassembler(bool parsing_correct, std::shared_ptr<DexParser> dex_parser, std::shared_ptr<DalvikOpcodes> dalvik_opcodes)
+        {
+            this->parsing_correct = parsing_correct;
+            this->dex_parser = dex_parser;
+            this->disassembly_correct = false;
+            this->dalvik_disassembler = std::make_shared<LinearSweepDisassembler>(dalvik_opcodes);
+        }
+
+        DexDisassembler::~DexDisassembler() {}
+
+        /**
+         * @brief get if the disassembly was correct or not.
+         * @return bool.
+         */
+        bool DexDisassembler::get_disassembly_correct()
+        {
+            return disassembly_correct;
+        }
+
+
+        /**
+         * @brief Public method for disassembly with error checking.
+         * @return void.
+         */
+        void DexDisassembler::disassembly_analysis()
+        {
+            if (!parsing_correct)
+                std::cerr << "[-] DEX was not correctly parsed, cannot disassembly it" << std::endl;
+            
+            try
+            {
+                this->disassembly_methods();
+                this->disassembly_correct = true;
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << "[-] Disassembly error: " << e.what() << '\n';
+                this->disassembly_correct = false;
+            }
+        }
+
+        /**
+         * @brief Get the linear sweep disassembler object.
+         * @return std::shared_ptr<LinearSweepDisassembler>
+         */
+        std::shared_ptr<LinearSweepDisassembler> DexDisassembler::get_linear_sweep_disassembler()
+        {
+            return dalvik_disassembler;
+        }
+
+        /**
+         * @brief Disassembly DEX methods using linear sweep disassembly, store instructions with its class and method.
+         * @return void.
+         */
+        void DexDisassembler::disassembly_methods()
+        {
+            auto dex_classes = dex_parser->get_classes();
+
+            for (size_t i = 0; i < dex_classes->get_number_of_classes(); i++)
+            {
+                // get class def
+                auto class_def = dex_classes->get_class_by_pos(i);
+
+                // get ClassDataItem
+                auto class_data_item = class_def->get_class_data();
+
+                // in case of interfaces, or classes with
+                // only virtual methods, no "classess_off"
+                // in the analysis.
+                if (class_data_item == nullptr)
+                    continue;
+
+                // now get direct method
+                // for each one we will start the disassembly.
+                for (size_t j = 0; j < class_data_item->get_number_of_direct_methods(); j++)
+                {
+                    auto direct_method = class_data_item->get_direct_method_by_pos(j);
+
+                    // get code item struct, here is the instruction buffer
+                    // here we will apply the disassembly.
+                    auto code_item_struct = direct_method->get_code_item();
+                    
+                    auto instructions = this->dalvik_disassembler->disassembly(code_item_struct->get_all_raw_instructions());
+
+                    this->method_instructions[{class_def, direct_method}] = instructions;
+                }
+            }
+        }
+
+
+        std::ostream& operator<<(std::ostream& os, const DexDisassembler& entry)
+        {
+            for (auto it = entry.method_instructions.begin(); it != entry.method_instructions.end(); it++)
+            {
+                auto class_def = std::get<0>(it->first);
+                auto direct_method = std::get<1>(it->first);
+
+                os << "Disassembly of method " << class_def->get_class_idx()->get_raw() << "->" << *direct_method->get_method()->get_method_name() << std::endl;
+
+                for (auto instr = it->second.begin(); instr != it->second.end(); instr++)
+                {
+                    os << std::right << std::setfill('0') << std::setw(8) << std::hex << instr->first << "  ";
+                    instr->second->give_me_instruction(os);
+                    os << std::endl;
+                }
+
+                os << std::endl;
+            }
+
+            return os;
+        }
+    }
+}
---
+++ DEX/DVM/dex_disassembler.hpp
@@ -0,0 +1,51 @@
+/**
+ * @file dex_disassembler.hpp
+ * @author Fare9
+ * 
+ * @brief Class to manage the disassembly process of
+ *        the DEX file.
+ */
+
+#ifndef DEX_DISASSEMBLER_HPP
+#define DEX_DISASSEMBLER_HPP
+
+#include <iostream>
+#include <memory>
+
+#include "dex_parser.hpp"
+#include "dex_instructions.hpp"
+#include "dex_linear_sweep_disassembly.hpp"
+
+namespace KUNAI
+{
+    namespace DEX
+    {
+        class DexDisassembler
+        {
+        public:
+            DexDisassembler(bool parsing_correct, std::shared_ptr<DexParser> dex_parser, std::shared_ptr<DalvikOpcodes> dalvik_opcodes);
+            ~DexDisassembler();
+
+            bool get_disassembly_correct();
+            void disassembly_analysis();
+            std::shared_ptr<LinearSweepDisassembler> get_linear_sweep_disassembler();
+            std::map<std::tuple<std::shared_ptr<ClassDef>, std::shared_ptr<EncodedMethod>>,
+                     std::map<std::uint64_t, std::shared_ptr<Instruction>>> get_instructions();
+
+            friend std::ostream& operator<<(std::ostream& os, const DexDisassembler& entry);
+        private:
+            void disassembly_methods();
+
+            std::shared_ptr<DexParser> dex_parser;
+            std::shared_ptr<LinearSweepDisassembler> dalvik_disassembler;
+            std::map<std::tuple<std::shared_ptr<ClassDef>, std::shared_ptr<EncodedMethod>>,
+                     std::map<std::uint64_t, std::shared_ptr<Instruction>>>
+                method_instructions;
+            bool parsing_correct;
+            bool disassembly_correct;
+
+        };
+    } // namespace DEX
+} // namespace KUNAI
+
+#endif
