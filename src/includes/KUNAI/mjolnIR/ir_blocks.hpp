9102a11e4332ba1a777e956c58a6b7c79e5c392f
Fare9 <eduardo.blazquez@uc3m.es>
2021-08-30 23:10:31 +0200

    Removed successors and predecessors from IRBlock

--- src/includes/KUNAI/mjolnIR/ir_blocks.hpp
+++ src/includes/KUNAI/mjolnIR/ir_blocks.hpp
@@ -23,42 +23,18 @@ namespace KUNAI {
         class IRBlock
         {
         public:
-            enum node_type_t
-            {
-                JOIN_NODE = 0,  // type of node with len(predecessors) > 1
-                BRANC_NODE,     // type of node with len(successors) > 1
-                REGULAR_NODE,   // other cases
-            };
-
             IRBlock();
             ~IRBlock();
 
             void append_statement_to_block(std::shared_ptr<IRStmnt> statement);
-            void add_block_to_sucessors(std::shared_ptr<IRBlock> successor);
-            void add_block_to_predecessors(std::shared_ptr<IRBlock> predecessor);
-
 
             bool delete_statement_by_position(size_t pos);
-            void delete_block_from_sucessors(std::shared_ptr<IRBlock> block);
-            void delete_block_from_precessors(std::shared_ptr<IRBlock> block);
 
             size_t get_number_of_statements();
             std::list<std::shared_ptr<IRStmnt>> get_statements();
-
-            size_t get_number_of_successors();
-            std::vector<std::shared_ptr<IRBlock>> get_successors();
-
-            size_t get_number_of_predecessors();
-            std::vector<std::shared_ptr<IRBlock>> get_predecessors();
-
-            node_type_t get_type_of_node();
         private:
             //! statements from the basic block.
             std::list<std::shared_ptr<IRStmnt>> block_statements;
-            //! successor blocks
-            std::vector<std::shared_ptr<IRBlock>> successors;
-            //! predecessor blocks
-            std::vector<std::shared_ptr<IRBlock>> predecessors;
         };
 
     }
--- src/mjolnIR/ir_blocks.cpp
+++ src/mjolnIR/ir_blocks.cpp
@@ -30,26 +30,6 @@ namespace KUNAI {
         }
 
         /**
-         * @brief Add a block as one of the successors of the current block.
-         * @param successor: successor block to add.
-         * @return void
-         */
-        void IRBlock::add_block_to_sucessors(std::shared_ptr<IRBlock> successor)
-        {
-            successors.push_back(successor);
-        }
-
-        /**
-         * @brief Add a block as on of the precessors of the current block.
-         * @param predecessor: predecessor block to add.
-         * @return void
-         */
-        void IRBlock::add_block_to_predecessors(std::shared_ptr<IRBlock> predecessor)
-        {
-            predecessors.push_back(predecessor);
-        }
-
-        /**
          * @brief Remove on of the statements given its position.
          * @param pos: position of the statement in the vector.
          * @return bool
@@ -87,30 +67,6 @@ namespace KUNAI {
         }
 
         /**
-         * @brief Remove one of the sucessors given an IRBlock, if it's inside of the vector.
-         * @param block: Block to remove from the successors.
-         * @return void
-         */
-        void IRBlock::delete_block_from_sucessors(std::shared_ptr<IRBlock> block)
-        {
-            auto position = std::find(successors.begin(), successors.end(), block);
-            if (position != successors.end())
-                successors.erase(position);
-        }
-
-        /**
-         * @brief Remove on of the predecessors given an IRBlock, if it's inside of the vector.
-         * @param block: Block to remove from the predecessors.
-         * @return void
-         */
-        void IRBlock::delete_block_from_precessors(std::shared_ptr<IRBlock> block)
-        {
-            auto position = std::find(predecessors.begin(), predecessors.end(), block);
-            if (position != predecessors.end())
-                predecessors.erase(position);
-        }
-
-        /**
          * @brief Get the number of statements from the block.
          * @return size_t
          */
@@ -126,56 +82,6 @@ namespace KUNAI {
         std::list<std::shared_ptr<IRStmnt>> IRBlock::get_statements()
         {
             return block_statements;
-        }
-
-        /**
-         * @brief Get the number of successor blocks from the current block.
-         * @return size_t
-         */
-        size_t IRBlock::get_number_of_successors()
-        {
-            return successors.size();
-        }
-
-        /**
-         * @brief Get the list of successor blocks.
-         * @return std::vector<std::shared_ptr<IRBlock>>
-         */
-        std::vector<std::shared_ptr<IRBlock>> IRBlock::get_successors()
-        {
-            return successors;
-        }
-
-        /**
-         * @brief Get the number of predecessor blocks from the current block.
-         * @return size_t
-         */
-        size_t IRBlock::get_number_of_predecessors()
-        {
-            return predecessors.size();
-        }
-
-        /**
-         * @brief Get the list of predecessor blocks.
-         * @return std::vector<std::shared_ptr<IRBlock>>
-         */
-        std::vector<std::shared_ptr<IRBlock>> IRBlock::get_predecessors()
-        {
-            return predecessors;
-        }
-
-        /**
-         * @brief Get type of node depending on number of successors and predecessors.
-         * @return node_type_t
-         */
-        IRBlock::node_type_t IRBlock::get_type_of_node()
-        {
-            if (successors.size() > 1)
-                return BRANC_NODE;
-            else if (predecessors.size() > 1)
-                return JOIN_NODE;
-            else
-                return REGULAR_NODE;
         }
     }
 }
