8a09c4ea3100a1f837c7643eb27af2a0e4143fab
Fare9 <eduardo.blazquez@uc3m.es>
2022-04-27 10:22:42 +0200

    Added some tests to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -26,3 +26,36 @@ if [ $? -eq 0 ]; then
 else
     echo "[-] Error passing test"
 fi
+
+echo "[+] Running 'test_ir_graph' test"
+
+echo "Log 'test_ir_graph'" >> ${LOG_FILE}
+./bin/projects/test_ir_graph ./tests/test-graph/Main.dex "LMain;" main "([Ljava/lang/String;)V" 2>> ${LOG_FILE}
+
+if [ -f "./main.dot" ]; then
+    echo "[!] Main.dot file was created correctly" >> ${LOG_FILE}
+    rm ./main.dot
+fi
+
+if [ $? -eq 0 ]; then
+    echo "[!] Test succeeded"
+else
+    echo "[-] Error passing test"
+fi
+
+echo "[+] Running 'test_dominators' test"
+
+echo "Log 'test_dominators' test" >> ${LOG_FILE}
+
+./bin/projects/test_dominators ./tests/test-graph/Main.dex "LMain;" main "([Ljava/lang/String;)V" 2>> ${LOG_FILE}
+
+if [ -f "./idom.dot" ]; then
+    echo "[!] idom.dot file was created correctly" >> ${LOG_FILE}
+    rm ./idom.dot
+fi
+
+if [ $? -eq 0 ]; then
+    echo "[!] Test succeeded"
+else
+    echo "[-] Error passing test"
+fi
