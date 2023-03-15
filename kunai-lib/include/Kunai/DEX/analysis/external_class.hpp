5db81bb813b16b83389aa97aceb12b214eee401f
Fare9 <eduardo.blazquez@uc3m.es>
2023-03-15 18:21:47 +0100

    Added class for external method and for external class of Dalvik

---
+++ kunai-lib/include/Kunai/DEX/analysis/external_class.hpp
@@ -0,0 +1,64 @@
+//--------------------------------------------------------------------*- C++ -*-
+// Kunai-static-analyzer: library for doing analysis of dalvik files
+// @author Farenain <kunai.static.analysis@gmail.com>
+// @author Ernesto Java <javaernesto@gmail.com>
+//
+// @file external_class.hpp
+// @brief A class for managing external classes that does not exist in
+// current DEX or current apk file
+
+#ifndef KUNAI_DEX_ANALYSIS_EXTERNAL_CLASS_HPP
+#define KUNAI_DEX_ANALYSIS_EXTERNAL_CLASS_HPP
+
+#include "Kunai/DEX/analysis/external_method.hpp"
+#include <iostream>
+#include <vector>
+#include <memory>
+
+namespace KUNAI
+{
+namespace DEX
+{
+    class ExternalClass
+    {
+        /// @brief name of the external class
+        std::string& name;
+        /// @brief Vector with all the external methods from the current class
+        std::vector<std::unique_ptr<ExternalMethod>> methods;
+    public:
+        ExternalClass(std::string& name) : name(name)
+        {}
+
+        /// @brief Get the name of the external class
+        /// @return name of the class
+        std::string& get_name() const
+        {
+            return name;
+        }
+
+        /// @brief Get a constant reference to the methods of the class
+        /// @return constant reference to methods
+        const std::vector<std::unique_ptr<ExternalMethod>>& get_methods() const
+        {
+            return methods;
+        }
+
+        /// @brief Get a reference to the methods of the class
+        /// @return reference to methods
+        std::vector<std::unique_ptr<ExternalMethod>>& get_methods()
+        {
+            return methods;
+        }
+
+        /// @brief Add an external method to the list of methods
+        /// @param method new method of the class
+        void add_external_method(std::unique_ptr<ExternalMethod>& method)
+        {
+            methods.push_back(std::move(method));
+        }
+    };
+} // namespace DEX
+} // namespace KUNAI
+
+
+#endif
