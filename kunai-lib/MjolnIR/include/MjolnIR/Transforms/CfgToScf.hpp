e114fadc135424cc4e9d24434081343df2a84062
Fare9 <eduardo.blazquez@uc3m.es>
2023-07-02 20:26:23 +0200

    Added references

--- kunai-lib/MjolnIR/include/MjolnIR/Transforms/CfgToScf.hpp
+++ kunai-lib/MjolnIR/include/MjolnIR/Transforms/CfgToScf.hpp
@@ -4,6 +4,11 @@
 //
 // @file CfgToScf.hpp
 // @brief Implementations for Kunai from the CfgToScf pass from numba project.
+// Numba project contains various presentations and slides:
+//  * Slides 2021: https://drive.google.com/file/d/114r8KHlPf1eyZiIX5ce8-ckm40xXzUzP/view
+//  * Presentation 2021: https://drive.google.com/file/d/1C6ecGtSK9-c_LuIT7CdHp-BQl3IBqpmV/view
+//  * Slides 2023: https://discourse.llvm.org/uploads/short-url/1EqK3iG8k3icbrHwHSZQA0sNQir.pdf
+//  * Repository: https://github.com/numba/numba-mlir
 
 #ifndef MJOLNIR_TRANSFORMS_CFGTOSCF_HPP
 #define MJOLNIR_TRANSFORMS_CFGTOSCF_HPP
