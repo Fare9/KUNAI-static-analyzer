b5bf34f26c6269e6bb9922afc9a24c8176f05449
Fare9 <eduardo.blazquez@uc3m.es>
2023-07-28 00:01:58 +0200

    Modified big switch for an unordered_map

--- kunai-lib/MjolnIR/lib/Lifter/InstructionLifter.cpp
+++ kunai-lib/MjolnIR/lib/Lifter/InstructionLifter.cpp
@@ -9,84 +9,65 @@
 
 using namespace KUNAI::MjolnIR;
 
+namespace
+{
+
+    typedef void (*generator_func)(KUNAI::MjolnIR::Lifter *, KUNAI::DEX::Instruction *);
+
+    template <class T>
+    void gen_instruction(KUNAI::MjolnIR::Lifter *lifter, KUNAI::DEX::Instruction *instr)
+    {
+        lifter->gen_instruction(reinterpret_cast<T>(instr));
+    }
+
+    template<>
+    void gen_instruction<KUNAI::DEX::PackedSwitch>(KUNAI::MjolnIR::Lifter *, KUNAI::DEX::Instruction *)
+    {
+        return;
+    }
+
+    template<>
+    void gen_instruction<KUNAI::DEX::SparseSwitch>(KUNAI::MjolnIR::Lifter *, KUNAI::DEX::Instruction *)
+    {
+        return;
+    }
+
+    std::unordered_map<KUNAI::DEX::dexinsttype_t, generator_func> generators = {
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION23X, &gen_instruction<KUNAI::DEX::Instruction23x *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION12X, &gen_instruction<KUNAI::DEX::Instruction12x *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION11X, &gen_instruction<KUNAI::DEX::Instruction11x *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22C, &gen_instruction<KUNAI::DEX::Instruction22c *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22T, &gen_instruction<KUNAI::DEX::Instruction22t *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION21T, &gen_instruction<KUNAI::DEX::Instruction21t *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION10T, &gen_instruction<KUNAI::DEX::Instruction10t *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION20T, &gen_instruction<KUNAI::DEX::Instruction20t *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION30T, &gen_instruction<KUNAI::DEX::Instruction30t *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION10X, &gen_instruction<KUNAI::DEX::Instruction10x *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION11N, &gen_instruction<KUNAI::DEX::Instruction11n *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION21S, &gen_instruction<KUNAI::DEX::Instruction21s *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION21H, &gen_instruction<KUNAI::DEX::Instruction21h *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION51L, &gen_instruction<KUNAI::DEX::Instruction51l *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION35C, &gen_instruction<KUNAI::DEX::Instruction35c *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION21C, &gen_instruction<KUNAI::DEX::Instruction21c *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22X, &gen_instruction<KUNAI::DEX::Instruction22x *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION32X, &gen_instruction<KUNAI::DEX::Instruction32x *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION31I, &gen_instruction<KUNAI::DEX::Instruction31i *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION31C, &gen_instruction<KUNAI::DEX::Instruction31c *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22S, &gen_instruction<KUNAI::DEX::Instruction22s *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22B, &gen_instruction<KUNAI::DEX::Instruction22b *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION31T, &gen_instruction<KUNAI::DEX::Instruction31t *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_PACKEDSWITCH, &gen_instruction<KUNAI::DEX::PackedSwitch *>},
+        {KUNAI::DEX::dexinsttype_t::DEX_SPARSESWITCH, &gen_instruction<KUNAI::DEX::SparseSwitch *>},
+    };
+}
+
 void Lifter::gen_instruction(KUNAI::DEX::Instruction *instr)
 {
-    switch (instr->get_instruction_type())
-    {
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION23X:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction23x *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION12X:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction12x *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION11X:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction11x *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22C:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction22c *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22T:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction22t *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION21T:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction21t *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION10T:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction10t *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION20T:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction20t *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION30T:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction30t *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION10X:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction10x *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION11N:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction11n *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION21S:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction21s *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION21H:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction21h *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION51L:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction51l *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION35C:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction35c *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION21C:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction21c *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22X:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction22x *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION32X:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction32x *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION31I:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction31i *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION31C:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction31c *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22S:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction22s *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION22B:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction22b *>(instr));
-        break;
-    case KUNAI::DEX::dexinsttype_t::DEX_INSTRUCTION31T:
-        gen_instruction(reinterpret_cast<KUNAI::DEX::Instruction31t *>(instr));
-        break;
-    /// no need to implement these
-    case KUNAI::DEX::dexinsttype_t::DEX_PACKEDSWITCH:
-    case KUNAI::DEX::dexinsttype_t::DEX_SPARSESWITCH:
-        break;
-    default:
+
+    auto fIt = ::generators.find(instr->get_instruction_type());
+
+    if (fIt != ::generators.end() && fIt->second != nullptr)
+        fIt->second(this, instr);
+    else
         throw exceptions::LifterException("MjolnIRLifter::gen_instruction: InstructionType not implemented");
-    }
 }
